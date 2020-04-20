f6c6e658ee7e98b35367df4378a21ab0513bc770
potatowagon <e0007652@u.nus.edu>
2020-04-20 09:19:40 +0800

    refrences to markers to reuse markers

--- client/client.js
+++ client/client.js
@@ -10,36 +10,28 @@ socket.on('disconnect', function () {
 
 var camData = [];
 var map = null;
-
-socket.on('googlemaps-key', function(key) {
-  gmKey = key;
-});
+var flagMarkers = {};
+var imgMarkers = {};
 
 socket.on('cam-data', function (cams) {
   camData = cams;
   console.log(cams);
-  setMarkers(map)
+  if (Object.keys(flagMarkers).length === 0){
+    initMarkerData(cams);
+  }
+  else {
+    updateMarkerImg(cams);
+  }
 });
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
+    zoom: 12,
     center: { lat: 1.35, lng: 103.8 }
   });
-
-  setMarkers(map);
 }
 
-
-function setMarkers(map) {
-  // Adds markers to the map.
-
-  // Marker sizes are expressed as a Size of X,Y where the origin of the image
-  // (0,0) is located in the top left of the image.
-
-  // Origins, anchor positions and coordinates of the marker increase in the X
-  // direction to the right and in the Y direction down.
-
+function initMarkerData(cams){
   var flag = {
     url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
     // This marker is 20 pixels wide by 32 pixels high.
@@ -57,6 +49,53 @@ function setMarkers(map) {
     coords: [1, 1, 1, 20, 18, 20, 18, 1],
     type: 'poly'
   };
+
+  zCounter = 0
+  cams.forEach(function(cam){
+    flagMarkers[cam.CameraID] = new google.maps.Marker({
+      position: { lat: cam.Latitude, lng: cam.Longitude },
+      map: map,
+      icon: flag,
+      shape: shape,
+      title: cam.CameraID,
+      zIndex: zCounter ++
+    });
+
+    var image = {
+      url: cam.ImageLink,
+      // This marker is 20 pixels wide by 32 pixels high.
+      size: new google.maps.Size(32, 24),
+      // The origin for this image is (0, 0).
+      origin: new google.maps.Point(0, 0),
+      // The anchor for this image is the base of the flagpole at (0, 32).
+      anchor: new google.maps.Point(0, 0)
+    };
+
+    imgMarkers[cam.CameraID] = new google.maps.Marker({
+      position: { lat: cam.Latitude, lng: cam.Longitude },
+      map: null,
+      icon: image,
+      title: cam.CameraID,
+      zIndex: zCounter ++
+    });
+  });
+}
+
+function updateMarkerImg(cams){
+  cams.forEach(function(cam){
+    imgMarkers[cam.CameraID].setIcon(cam.ImageLink);
+  });
+}
+
+
+function setMarkers(map) {
+  // Adds markers to the map.
+
+  // Marker sizes are expressed as a Size of X,Y where the origin of the image
+  // (0,0) is located in the top left of the image.
+
+  // Origins, anchor positions and coordinates of the marker increase in the X
+  // direction to the right and in the Y direction down.
 
   for (var i = 0; i < camData.length; i++) {
     var cam = camData[i];
